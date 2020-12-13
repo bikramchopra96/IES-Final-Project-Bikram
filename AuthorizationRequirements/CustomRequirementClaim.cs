28eb4f9dc32a3bd80d36a158f159e6f3dc59ed4f
bikramchopra96 <71872366+bikramchopra96@users.noreply.github.com>
2020-12-13 22:05:15 +0530

    Add files via upload

---
+++ AuthorizationRequirements/CustomRequirementClaim.cs
@@ -0,0 +1,45 @@
+﻿/*using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basics.AuthorizationRequirements
+{
+    public class CustomRequirementClaim : IAuthorizationRequirement
+    {
+        public CustomRequirementClaim(string claimType)
+        {
+            ClaimType = claimType;
+        }
+        public string ClaimType { get; }
+
+        public class CustomRequirementClaimHandler : AuthorizationHandler<CustomRequirementClaim>
+        {
+            protected override Task HandleRequirementAsync(
+                AuthorizationHandlerContext context,
+                CustomRequirementClaim requirement)
+            {
+                var hasClaim = context.User.Claims.Any(x => x.Type == requirement.ClaimType);
+
+                if (hasClaim)
+                {
+                    context.Succeed(requirement);
+                }
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+    public static class AuthorizationPolicyBuilderExtensions
+    {
+        public static AuthorizationPolicyBuilder RequirementCustomClaim(
+            this AuthorizationPolicyBuilder builder,
+            string claimType)
+        {
+            builder.AddRequirements(new CustomRequirementClaim(claimType));
+            return builder;
+        }
+    }
+}
+*/
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using Basics.CustomPolicyProvider;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAuthorizationService _authorizationService;
+        public HomeController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Claim.DoB")]
+        public IActionResult SecretPolicy()
+        {
+            return View("Secret");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult SecretRole()
+        {
+            return View("Secret");
+        }
+
+        [SecurityLevel(5)]
+        public IActionResult SecretLevel()
+        {
+            return View("Secret");
+        }
+
+        [SecurityLevel(10)]
+        public IActionResult SecretHigherLevel()
+        {
+            return View("Secret");
+        }
+
+        [AllowAnonymous]
+        public IActionResult Authenticate()
+        {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "bob@email.com"),
+                new Claim(ClaimTypes.DateOfBirth, "26/05/1995"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "AdminTwo"),
+                new Claim(DynamicPolicies.SecurityLevel, "7"),
+                new Claim("Grandma.says", "Very Nice boi."),
+            };
+
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob K Foo"),
+                new Claim("DrivingLicense", "A+"),
+            };
+
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licesneIdentity = new ClaimsIdentity(licenseClaims, "Government");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity , licesneIdentity});
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+
+        }
+
+        public async Task<IActionResult> DoStuff(
+            [FromServices] IAuthorizationService authorizationService)
+        {
+           
+            var builder = new AuthorizationPolicyBuilder("Schema");
+            var customPolicy = builder.RequireClaim("Hello").Build();
+
+            var authResult = await authorizationService.AuthorizeAsync(User, customPolicy);
+
+            if (authResult.Succeeded)
+            {
+                return View("Index");
+            }
+
+            return View("Index");
+        }
+    }
+}
